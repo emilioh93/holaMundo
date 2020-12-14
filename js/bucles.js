b3b34c3754e0daa75148b5115b8de51ff0db4c98
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-14 17:22:44 -0300

    escribí bucle while

---
+++ js/bucles.js
@@ -0,0 +1,14 @@
+//While
+/*
+while(condición lógica){
+    ejecutar tal código
+    cambié la condición
+}
+*/
+
+let contador = 1;
+
+while (contador <= 15) {
+    document.write("<br>Elemento " + contador);
+    contador++
+}
