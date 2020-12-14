854574fae4770237b4198e4ff07dfd8f524320d1
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-14 17:42:31 -0300

    escribí bucle do while

--- js/bucles.js
+++ js/bucles.js
@@ -12,3 +12,13 @@ while (contador <= 15) {
     document.write("<br>Elemento " + contador);
     contador++; //La otra opción es: contador = contador + 1; o contador += 1
 }
+
+
+//Do while
+//Hacer...mientras se cumple la condición lógica
+let cont = 16;
+
+do {
+    document.write("<br>Vacaciones faltan " + cont + " días");
+    cont--;
+} while (cont >= 1)
