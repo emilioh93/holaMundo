464b357b68245019af263f822353e85029175f48
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-16 17:19:26 -0300

    terminamos arreglos

--- js/arreglos.js
+++ js/arreglos.js
@@ -13,3 +13,35 @@ console.log(series.length);
 for (let i = 0; i < series.length; i++) {
     document.write(series[i] + " - ");
 }
+
+// Agregar un elemento al final del arreglo
+series.push("Peaky Blinders");
+document.write("<h4>Agregar un elemento al final del arreglo</h4>")
+console.log(series.length);
+for (let i = 0; i < series.length; i++) {
+    document.write(series[i] + " - ");
+}
+
+// Agregar elemento a una posición particular del arreglo
+series.splice(1, 0, "Gambito de Dama");
+document.write("<h4>Agregar un elemento a una posición particular del arreglo</h4>")
+console.log(series.length);
+for (let i = 0; i < series.length; i++) {
+    document.write(series[i] + " - ");
+}
+
+// Eliminar elementos desde una posición de un arreglo
+series.splice(4, 2);
+document.write("<h4>Eliminar elementos desde una posición particular del arreglo</h4>")
+console.log(series.length);
+for (let i = 0; i < series.length; i++) {
+    document.write(series[i] + " - ");
+}
+
+// Modificar elemento de mi arreglo
+series[0] = "Dark";
+document.write("<h4>Modificar elemento de mi arreglo</h4>")
+console.log(series.length);
+for (let i = 0; i < series.length; i++) {
+    document.write(series[i] + " - ");
+}
