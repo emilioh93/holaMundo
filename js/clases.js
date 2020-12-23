f5101a7fe06d64c0302cd158469c5a7cca3efc11
Emilio Hurtado <emiliohurtado1@gmail.com>
2020-12-23 18:35:35 -0300

    terminé herencia

--- js/clases.js
+++ js/clases.js
@@ -44,6 +44,28 @@ class Persona {
     }
 }
 
+// Herencia
+class Alumno extends Persona {
+    constructor(nombreP, apellidoP, mailP, anioP, usuarioP, passP, legajoP, estadoCursadoP, cursoP) {
+            super(nombreP, apellidoP, mailP, anioP, usuarioP, passP);
+            this.legajo = legajoP;
+            this.curso = cursoP;
+            this.estadoCursado = estadoCursadoP;
+        }
+        // Métodos
+    mostrarDatos() {
+        document.write(`<h4>Persona</h4>
+        Nombre: ${this.nombre}
+        <br>Apellido: ${this.apellido}
+        <br>Mail: ${this.mail}
+        <br>Año de alta: ${this.anio}
+        <br>Nombre de usuario: ${this.usuario}
+        <br>Estado: ${this.estado}
+        <br>Legajo: ${this.legajo}
+        <br>Curso: ${this.curso}`);
+    }
+}
+
 // Crear objetos
 let pablo = new Persona("Pablo", "Domínguez", "pablodominguez@gmail.com", 2019, "pablodominguez", "12345678");
 let vero = new Persona("Verónica", "Auad", "veroauad@gmail.com", 2020, "veroauad", "12345678");
@@ -56,3 +78,7 @@ vero.mostrarDatos();
 document.write("<br>" + vero.mostrarMail);
 vero.modificarMail = "veronica@mail.com";
 document.write("<br>" + vero.mostrarMail);
+
+// Crear objeto Alumno
+let esteban = new Alumno("Esteban", "Velardez", "esteban.velardez@gmail.com", 2020, "evelardez", "123", 1546, "En proceso", "FullStack");
+esteban.mostrarDatos();
